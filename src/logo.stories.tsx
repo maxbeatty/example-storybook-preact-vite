ea1a5a0c937c7c10826df91ddae34467d5013d71
Max Beatty <mbeatty@squareup.com>
2022-05-18 16:33:51 -0700

    ReferenceError: h is not defined

---
+++ src/logo.stories.tsx
@@ -0,0 +1,13 @@
+/** @jsx h */
+import { h } from "preact";
+
+import { Logo } from "./logo";
+
+export default {
+  title: "Example/Logo",
+  component: Logo,
+};
+
+const Template = () => <Logo />;
+
+export const Default = Template.bind({});
